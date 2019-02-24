b756d3831a4907960890ba9ee3b8f2451aed3a8b
yongfei huo <hyfpersonal@outlook.com>
2019-02-24 09:44:43 +0800

    init useState

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const Count = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello React Hooks.</h1>
+      <h2>1. useState</h2>
+      <Count />
     </div>
   );
 }
